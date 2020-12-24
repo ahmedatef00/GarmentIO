272c0423b984d631894dabfee0d3b3054b68eb88
ahmedatef00 <ahmedatef62437@gmail.com>
2020-12-24 21:23:40 +0200

    final touches

--- src/batches/validation/index.js
+++ src/batches/validation/index.js
@@ -1,21 +1,39 @@
+import {
+    number
+} from 'yargs';
+import batch from '../../Models/batch';
+
 const {
     body,
 } = require('express-validator')
 
 export let batchValidation = [
-    body('number', 'must be a numeric value')
-    .notEmpty()
-    .isNumeric(),
+    body('number')
+    .exists()
+    .withMessage('number is required')
+    .custom(value => {
+        return batch.findOne({
+            number: value
+        }).then(number => {
+            if (number!==null) {
+                return Promise.reject('Number is already in use');
+            }
+        })
+    })
+    .isNumeric()
+    .withMessage('number  must be Numeric'),
+
+
     body('size', 'must be in [S,X,L,X]')
     .notEmpty()
     .isIn(['S', 'M', 'L', 'X']),
 
-    body('color', 'must be in ["red", "blue", "black", "green"]')
+    body('color', 'must be in [red, blue, black, green]')
     .notEmpty()
     .isIn(["red", "blue", "black", "green"]),
 
-    body('quantity', 'must be a numeric value').
-    notEmpty()
-    .isNumeric()
+    body('quantity').
+    exists().withMessage('quantity is required')
+    .isNumeric().withMessage(' quantity must be a numeric value')
 
 ];
